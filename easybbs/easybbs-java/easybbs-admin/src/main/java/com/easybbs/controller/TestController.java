b61fd68bf34dd830aad029b2452d2f806bb2b35e
王帅豪 <wsh82898@163.com>
2023-04-01 21:19:12 +0800

    引入糊涂工具包

--- easybbs/easybbs-java/easybbs-admin/src/main/java/com/easybbs/controller/TestController.java
+++ easybbs/easybbs-java/easybbs-admin/src/main/java/com/easybbs/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.easybbs.controller;
 
+import cn.hutool.core.util.RandomUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/test")
     public String test(){
-        return "hello word Admin 1111 !!!!";
+        int random = getRandom(1, 100);
+        return String.valueOf(random);
+    }
+
+
+    /**
+     * 获取一个随机数
+     * @param min 最小区间
+     * @param max 最大区间
+     * @return
+     */
+    public int getRandom(int min, int max){
+        int i = RandomUtil.randomInt(min,max);
+        return i;
     }
 }
